371f83c8fd1d4288c3515aa486c54460126176c4
bhargav2520 <bhargav2520@gmail.com>
2025-07-29 12:50:38 -0400

    idexcel learning week 3 learning web devlopment

---
+++ week 4 learning web devlopment/src/main/java/com/franklin/techblog/FranklinTechBlogApplication.java
@@ -0,0 +1,11 @@
+package com.franklin.techblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class FranklinTechBlogApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(FranklinTechBlogApplication.class, args);
+    }
+}
